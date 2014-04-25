f46af02760c12ac4c780939cf514c5a45e7bc694
fedora <git@mangoraft.com>
2014-04-25 16:25:03 -0400

    rewrite router to use hipache

--- bin/route-server.js
+++ bin/route-server.js
@@ -1,15 +1,30 @@
-#!/usr/bin/env node
+var cluster = require('cluster');
 
-var raft = require('raft');
-var path = require('path');
-var Router = require('../');
+var master = require('../lib/route-machine/master');
+var worker = require('../lib/route-machine/worker');
 
-raft.common.printLogo();
+var config = {
+	"server" : {
+		"debug" : false,
+		"accessLog" : "/tmp/proxy2_test_access.log",
+		"port" : 1080,
+		"workers" : 2,
+		"maxSockets" : 200,
+		"deadBackendTTL" : 30,
+		"tcpTimeout" : 5,
+		"retryOnError" : 2,
+		"deadBackendOn500" : true,
+		"httpKeepAlive" : false
+	}
+};
 
-raft.once('start', function() {
-	var rotuer = new Router();
-	rotuer.start();
-});
-
-
-raft.start(process.argv[2]);
+if (cluster.isMaster) {
+	// Run the master
+	var m = master(config);
+	m.run();
+	console.log('Server is running. ' + JSON.stringify(config.server));
+} else {
+	// Run the worker
+	var w = worker(config);
+	w.run();
+}
