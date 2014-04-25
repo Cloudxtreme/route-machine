f170242c9bd8c99cd100681fbeadb3ca978f5130
fedora <git@mangoraft.com>
2014-04-25 16:41:43 -0400

    removed node_module folder

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /**
  * Raft
- * 
+ *
  * route-machine
- * 
+ *
  * HTTP/WS router.
  *
  */
 
-module.exports = require('./lib/route-machine/router');
+module.exports.Master = require('./lib/route-machine/master');
+module.exports.Worker = require('./lib/route-machine/worker');
+module.exports.MemoryMonitor = require('./lib/route-machine/memorymonitor');
+module.exports.master = require('./lib/route-machine/router'); 
